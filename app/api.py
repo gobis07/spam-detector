46668e5b7855be96d4d506c8dce57455392e7e0b
Gobis07 <bisimwagloire071@gmail.com>
2026-05-16 10:28:05 +0200

    added sqlite3

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-
+import sqlite3
 app = FastAPI()
 model = joblib.load("model/spam_model.pkl")
 
@@ -11,8 +11,26 @@ class Message(BaseModel):
 @app.post("/predict")
 def predict(data:Message):
 
-    prediction = model.predict([data.text])
-
+    prediction = model.predict([data.text])[0]
+    conn = sqlite3.connect("message.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO message(text,prediction)
+        VALUES (?,?)
+        """,
+        (data.text,prediction)
+    )
+    conn.commit()
+    conn.close()
     return{
-        "prediction":prediction[0]
+        "prediction":prediction
     }
+@app.get("/message")
+def get_messages():
+    conn = sqlite3.connect("message.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM message")
+    message = cursor.fetchall()
+    conn.close()
+    return{"message":message}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import requests
+import subprocess
+
+#subprocess.Popen(["uvicorn","app.api:app","--reload"])
 st.title("Spam Detection App")
 st.write("Entrez un message pour voir s'il s'agit d'un spam ou non.")
 
 user_input = st.text_area("Message")
-
+#-------------------------Prediction -----------------------------------------------------
 if st.button("Predire"):
     response = requests.post(
         "http://127.0.0.1:8000/predict",
@@ -16,3 +19,18 @@ if st.button("Predire"):
         st.error("Spam détecter")
     else:
         st.success("Not spam")
+
+
+#---------------------------historique-----------------------------------------------------
+st.subheader("message history")
+
+history = requests.get(
+    "http://127.0.0.1:8000/message"
+)
+message = history.json()['message']
+for msg in message:
+    st.write(f"""
+            Message : {msg[1]}
+            Prediction : {msg[2]}
+            Date : {msg[3]}
+""")
