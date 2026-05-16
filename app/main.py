24d6d44fdf6119ec1077506d3305d4f5d1dd50fd
Gobis07 <bisimwagloire071@gmail.com>
2026-05-16 10:34:38 +0200

    added sqlite3

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 import streamlit as st
 import requests
 import subprocess
+import time
 
 #subprocess.Popen(["uvicorn","app.api:app","--reload"])
+time.sleep(2)
 st.title("Spam Detection App")
 st.write("Entrez un message pour voir s'il s'agit d'un spam ou non.")
 
 user_input = st.text_area("Message")
 #-------------------------Prediction -----------------------------------------------------
 if st.button("Predire"):
-    response = requests.post(
-        "http://127.0.0.1:8000/predict",
-        json={"text":user_input}
-    )
-    result = response.json()
-    prediction = result["prediction"]
-    if prediction == "spam":
-        st.error("Spam détecter")
-    else:
-        st.success("Not spam")
+    try:
+        response = requests.post(
+            "http://127.0.0.1:8000/predict",
+            json={"text":user_input}
+        )
+        result = response.json()
+        prediction = result["prediction"]
+        if prediction == "spam":
+            st.error("Spam détecter")
+        else:
+            st.success("Not spam")
+    except requests.exceptions.ConnectionError:
+        st.error("L'API est eteint")
+        
 
 
 #---------------------------historique-----------------------------------------------------
